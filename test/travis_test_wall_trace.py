150356501a59273dab4054c9c761d1a2876d2a42
aayaka <aayaka0719@gmail.com>
2017-08-03 14:09:55 +0900

    1409

--- test/travis_test_wall_trace.py
+++ test/travis_test_wall_trace.py
@@ -20,21 +20,21 @@ class WallStopTest(unittest.TestCase):
 		left, right = self.set_and_get(400, 100, 100, 0) #total: 600
 		self.assertTrue(left == right == 0, "can't stop")
 
-		left, right = self.set_and_get(40, 0, 0, 9) #total: 49
-		self.assertTrue(0 < left == right < 1000, "can't move agein")
+		left, right = self.set_and_get(0, 5, 1000, 0) #total: 49
+		self.assertTrue(left == right != 0, "stop wrongly by side sensors")
 
-		time.sleep(5.0)
-		left, right =self.set_and_get(40, 0, 0, 9)
-		self.assertTrue(2000 < left == right, "cant accerelate")
+		left, right =self.set_and_get(0, 10, 0, 0)
+		self.assertTrue(left < right, "dont curve to left")
 
+		left, right = self.set_and_get(0, 0, 200, 0) #total: 500
+		self.assertTrue(left > right, "dont curve to right")
 
-		left, right = self.set_and_get(15, 0, 20, 15) #total: 500
-		self.assertTrue(left == right == 0, "can,t stop agein")
-
+		left, right = self.set_and_get(0,5,0,0)
+		self.assertTrue(0 < left == right, "curve wrongly")
 
 
 if __name__ == '__main__':
 	time.sleep(3)
-	rospy.init_node('travis_test_wall_stop_accel')
-	rostest.rosrun('pimouse_run_corridor','travis_test_wall_stop_accel',WallStopTest)
+	rospy.init_node('travis_test_wall_trace')
+	rostest.rosrun('pimouse_run_corridor','travis_test_wall_trace',WallStopTest)
 
